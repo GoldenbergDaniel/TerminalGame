4058179c90668b7df760b80b2548eec4f7b59ced
GoldenbergDaniel <danielgoldenberg0@outlook.com>
2024-04-22 01:35:38 -0400

    Rewrote windows build script

--- build_macos.sh
+++ build_macos.sh
@@ -1,22 +1,26 @@
 NAME="game"
 MODE=$1
 
+RLS_FLAGS="-o:speed -lld -microarch:native -disable-assert -no-type-assert
+  -no-bounds-check -vet -warnings-as-errors"
+
+DBG_FLAGS="-o:none -lld -debug -sanitize=address"
+
+DEV_FLAGS="-o:none -lld"
+
 if [[ $MODE == "r" || $MODE == "-r" ]]
 then
   echo "Building macOS release..."
-  odin build src -out=$NAME -lld -o:speed -microarch:native -disable-assert -no-type-assert \
-  -no-bounds-check -vet -warnings-as-errors
+  odin build src -out=$NAME $REL_FLAGS
 elif [[ $MODE == "d" || $MODE == "-d" ]]
 then
   echo "Building macOS debug..."
   mkdir debug
   pushd debug
-  odin build ../src -out=$NAME -lld -o:none -debug -sanitize=address
+  odin build ../src -out=$NAME 
   popd
 else
   echo "Building macOS dev..."
-  odin build src -out=$NAME -lld -o:none
+  odin build src -out=$NAME 
   ./$NAME
 fi
-
-echo "Done!"
--- build_windows.bat
+++ build_windows.bat
@@ -1,5 +1,27 @@
-@echo Compiling project...
-@odin build src -out=Game.exe -no-crt -use-separate-modules -o:none -show-timings
-@REM @odin build src -out=debug\Game.exe -debug -no-crt -o:none
-@REM @odin build src -out=Game.exe -o:speed -no-crt -no-bounds-check -disable-assert
-@echo Compilation complete.
+@echo off
+setlocal
+
+set MODE=%1%
+
+set RLS_FLAGS=-o:speed -microarch:native -disable-assert -no-bounds-check -no-crt -vet -warnings-as-errors
+
+set DBG_FLAGS=-o:none -debug
+
+set DEV_FLAGS=-o:none -no-crt -use-separate-modules
+
+if "%MODE%"=="d" (
+  echo Building windows debug...
+  mkdir debug
+  pushd debug
+  odin build ../src -out=game.exe %DBG_FLAGS%
+  popd
+) else (
+  if "%MODE%"=="r" (
+    echo Building windows release...
+    odin build src -out=game.exe %RLS_FLAGS%
+  ) else (
+    echo Building windows dev...
+    odin build src -out=game.exe %DEV_FLAGS%
+    game.exe
+  )
+)
